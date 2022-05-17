053e113b16c1f7eef95fd68dc162b9be43ebea3e
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-17 12:21:06 -0300

    Initial commit

---
+++ Assets/Vector3Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour
+{
+    public Vector3 myVector3;
+    public GameObject myObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myObject.transform.eulerAngles = myVector3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Vector3-y-Transform-4to-B
+ 
